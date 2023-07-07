fd1b3c96d7d753c374693ea243d61e53ba48c84a
norayrmayil <mayilyannorayr515@gmail.com>
2023-07-07 19:49:14 +0400

    changing all files and adding car models

--- main.js
+++ main.js
@@ -1,36 +1,68 @@
-
-let seconds = 10;
+let correct;
+let seconds = 120;
 let correctAnswer = 0;
 let incorrectAnswer = 0;
-
 function getElement(id) {
-  return document.getElementById(id);
+    return document.getElementById(id);
+}
+function getRandomCountry() {
+    return models[Math.round(Math.random() * (models.length - 1))];
+}
+function main() {
+    let options = [];
+    const maxOptions = 3;
+    while (options.length < maxOptions) {
+        let coun = getRandomCountry();
+        if (options.indexOf(coun) === -1) {
+            options.push(coun);
+        }
+    }
+    for (let i = 0; i < options.length; i++) {
+        getElement(`option${i + 1}label`).innerHTML = options[i].name;
+        getElement(`option${i + 1}input`).value = options[i].name;
+        getElement(`option${i + 1}input`).checked = false;
+    }
+    correct = options[Math.round(Math.random() * (options.length - 1))];
+    getElement("flag").src = correct.model;
 }
 function timer() {
- setTimeout(finish, seconds * 1000);
-  getElement("time").innerHTML = seconds;
-  let countdown = setInterval(function () {
-    seconds--;
-    getElement("time").textContent = seconds;
-    if (seconds <= 0) clearInterval(countdown);
-    if (seconds === 5) getElement("time").style.color = "#ff0000";
-  }, 1000);
+    setTimeout(finish, seconds * 1000);
+    getElement("time").innerHTML = seconds;
+    let countdown = setInterval(function () {
+        seconds--;
+        getElement("time").textContent = seconds;
+        if (seconds <= 0) clearInterval(countdown);
+        if (seconds === 5) getElement("time").style.color = "#ff0000";
+    }, 1000);
 }
 function check() {
-  let input;
-  try {
-    input = document.querySelector('input[name = "option"]:checked').value;
-  } catch {
-    return;
-  }
-  correctAnswer++;
-  getElement("score").innerHTML = correctAnswer;
-   clearInterval(checkInterval);
+    let input;
+    try {
+        input = document.querySelector('input[name = "option"]:checked').value;
+    } catch {
+        return;
+    }
+    if (input === correct.name) {
+        correctAnswer++;
+        getElement("score").innerHTML = correctAnswer;
+    } else {
+        incorrectAnswer++;
+    }
+    main();
 }
 function finish() {
-   clearInterval(checkInterval);
-  let percentage = 100;
-  getElement("alertaccuracy").innerHTML = `der ardyunqn e ${percentage}%`;
+    clearInterval(checkInterval);
+    let percentage = Math.round(correctAnswer / (incorrectAnswer + correctAnswer) * 100);
+    let resultForAnswers;
+    if (percentage >= 75) {
+        resultForAnswers = "gerazanc"
+    }
+    // else if (){
+
+    // }
+    getElement("alertaccuracy").innerHTML = `duq cucaberel eq ${resultForAnswers} ardyunq `;
 }
+
 let checkInterval = setInterval(check, 50);
+main();
 timer();
--- models.js
+++ models.js
@@ -1,6 +1,71 @@
 const models = [
     {
-      "car": "./img/m5.jpg",
-      "name": "BMW M5 F90"
+        "model": "model/cls63.jpg",
+        "name": "Mercedes-Benz CLS 63 AMG"
     },
-  ]
+    {
+        "model": "model/e63.jpg",
+        "name": "Mercedes-Benz E 63 AMG"
+    },
+    {
+        "model": "model/gt63.jpg",
+        "name": "Mercedes-Benz GT 63 AMG"
+    },
+    {
+        "model": "model/cls55.jpg",
+        "name": "Mercedes-Benz CLS 55 AMG"
+    },
+    {
+        "model": "model/priora.jpg",
+        "name": "Lada Priora 2018"
+    },
+    {
+        "model": "model/chiron.jpg",
+        "name": "Bugatti Chiron Sport"
+    },
+    {
+        "model": "model/m4.jpg",
+        "name": "BMW M4 Competition"
+    },
+    {
+        "model": "model/g63.jpg",
+        "name": "Mercedes-Benz G63 AMG"
+    },
+    {
+        "model": "model/mcp1.jpg",
+        "name": "McLaren P1"
+    },
+    {
+        "model": "model/crossland.jpg",
+        "name": "Opel Crossland 2022"
+    },
+    {
+        "model": "model/m5cs.jpg",
+        "name": "BMW M5 CS"
+    },
+    {
+        "model": "model/purosangue.jpg",
+        "name": "Ferrari Purosangue"
+    },
+    {
+        "model": "model/bentayga.jpg",
+        "name": "Bentley Bentayga"
+    },
+    {
+        "model": "model/m3.jpg",
+        "name": "BMW M3 CSL"
+    },
+    {
+        "model": "model/urus.jpg",
+        "name": "Lamborghini Urus"
+    },
+    {
+        "model": "model/granta.jpg",
+        "name": "Lada Granta"
+    },
+    {
+        "model": "model/lafer.jpg",
+        "name": "Ferrari La Ferrari"
+    },
+    
+]
